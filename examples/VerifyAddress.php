afd60f896c248c9b6188e5934fb2b35e618ca458
rk <robert@woodst.com>
2019-06-13 16:14:30 -0500

    First updated example

--- examples/VerifyAddress.php
+++ examples/VerifyAddress.php
@@ -1,27 +1,29 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Tony DeStefano
- * Date: 10/22/15
- * Time: 2:13 PM
+ * Copyright © 2019 by Wood Street, Inc. All Rights reserved.
  */
 
-require_once('config.php');
+require_once 'config.php';
+
+use rk\Taxify\Communicator;
+use rk\Taxify\Requests\VerifyAddress;
+use rk\Taxify\Taxify;
+
+$request = new VerifyAddress();
+
+$request
+    ->setStreet1('16201 E Indiana St')/* should change St to Ave */
+    ->setCity('Spokane Valley')
+    ->setRegion('WA')
+    ->setPostalCode('99216');
 
 try {
     /* initialize taxify */
-    $taxify = new ZayconTaxify\Taxify(API_KEY, ZayconTaxify\Taxify::ENV_DEV, true);
+    $taxify = new Taxify(API_KEY, Taxify::ENV_PROD, true);
+    $comm   = new Communicator($taxify);
+    $result = $request->execute($comm);
 
-    /* address */
-    $address = new ZayconTaxify\Address($taxify);
-    $address
-        ->setStreet1('16201 E Indiana St')/* should change St to Ave */
-        ->setCity('Spokane Valley')
-        ->setState('WA')
-        ->setPostalCode('99216')
-        ->verifyAddress();
-
-    var_dump($address);
-} catch (ZayconTaxify\Exception $e) {
+    var_dump($result);
+} catch (Exception $e) {
     var_dump($e);
 }
